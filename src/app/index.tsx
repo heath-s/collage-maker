703fbde5e568a198d304f7245569a5f5bc394647
신희성 (Heath) <5037055+lumiere0b@users.noreply.github.com>
2021-03-12 14:23:28 +0900

    feat(app): 예제 App 대체

---
+++ src/app/index.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from 'react';
+import styles from './index.module.scss';
+
+const App: FunctionComponent = () => {
+  return (
+    <main className={styles.container}>
+      <header>
+        <h1>Collage Maker</h1>
+      </header>
+
+      <section>
+        <p>TODO</p>
+      </section>
+    </main>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import App from './app';
 import store from './app/store';
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
